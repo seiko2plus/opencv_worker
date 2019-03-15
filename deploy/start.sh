78f2d41eca4781931828933c766379b041ae7ea8
Sayed Adel <seiko@imavr.com>
2019-03-15 10:32:42 +0200

    sync repos before worker start

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+/worker/deploy/sync.sh
 spawn-fcgi -u buildbot -g buildbot -M 666 -s /worker/nginx/fcgiwrap.socket /usr/sbin/fcgiwrap
 cd /worker/buildbot && buildbot-worker start --nodaemon
